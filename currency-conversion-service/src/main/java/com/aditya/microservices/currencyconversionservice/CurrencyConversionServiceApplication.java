918c252b3b4d38f62f0482ce2ae22440df21daa3
Aditya Rahman Sidqi <adityars.sidqi@gmail.com>
2020-09-16 22:30:18 +0700

    implement ribbon, eureka, feign, and client load balancing

---
+++ currency-conversion-service/src/main/java/com/aditya/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,18 @@
+package com.aditya.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients("com.aditya.microservices.currencyconversionservice")
+@EnableEurekaClient
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
